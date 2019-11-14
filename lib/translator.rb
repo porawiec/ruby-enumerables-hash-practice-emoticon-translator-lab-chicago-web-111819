9e1bbafd8ce40b9b7504c7fa5d1f8c499b850f95
Paul O <paul.orawiec@gmail.com>
2019-11-14 09:45:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_path)
     emotes["get_meaning"][japanese] = meaning
     emotes["get_emoticon"][english] = japanese
   end
-  pp emotes
+  emotes
 end
 
 def get_japanese_emoticon
