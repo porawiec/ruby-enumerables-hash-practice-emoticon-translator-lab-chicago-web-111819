d939221fee78cf9f51a8cc40140bfc426c73fbc0
Paul O <paul.orawiec@gmail.com>
2019-11-14 09:29:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,12 @@ require "yaml"
 def load_library(file_path)
   emotes = {"get_meaning" => {}, "get_emoticon" => {} }
   YAML.load_file(file_path).each do | meaning, ej_array |
-    p english, japanese = ej_array
+    english, japanese = ej_array
 
   end
+  p ej_array
+  p english
+  p japanese
   emotes
 end
 
